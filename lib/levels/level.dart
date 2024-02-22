3039a7d5825f96fb9e0dd0bde1db633c86166376
heeseung <ys2645@naver.com>
2024-02-22 21:38:26 +0900

    3회차, 메인 캐릭터 움직임, 컨트롤 장치 추가 (조이스틱)

--- lib/levels/level.dart
+++ lib/levels/level.dart
@@ -4,9 +4,12 @@ import 'package:flame/components.dart';
 import 'package:flame_tiled/flame_tiled.dart';
 import 'package:game_demo1/actors/player.dart';
 
+
+
 class Level extends World{
   final String levelName;
-  Level({required this.levelName});
+  final Player player;
+  Level({required this.levelName, required this.player});
 
   late TiledComponent level;
 
@@ -21,10 +24,7 @@ class Level extends World{
     for(final spawnPoint in spawnPointsLayer!.objects){
       switch(spawnPoint.class_){
         case 'Player':
-          final player = Player(
-            character: 'Mask Dude',
-            position: Vector2(spawnPoint.x, spawnPoint.y),
-          );
+          player.position = Vector2(spawnPoint.x, spawnPoint.y);
           add(player);
           break;
         default:
--- lib/main.dart
+++ lib/main.dart
@@ -12,15 +12,16 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:game_demo1/pixel_adventure.dart';
 
-void main() {
+void main() async{
   // SharedPreferences 등 비동기로 데이터를 다룬 다음
   // runApp을 실행해야하는 경우 아래 한줄을 반드시 추가해야합니다.
   // WidgetsFlutterBinding.ensureInitialized();
   WidgetsFlutterBinding.ensureInitialized();
-  Flame.device.fullScreen();
-  Flame.device.setLandscape();
+  await Flame.device.fullScreen();
+  await Flame.device.setLandscape();
 
   PixelAdventure game = PixelAdventure();
-  runApp(GameWidget(game: kDebugMode ? PixelAdventure() : game)
+  runApp(
+    GameWidget(game: kDebugMode ? PixelAdventure() : game),
   );
 }
